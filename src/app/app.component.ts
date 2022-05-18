f01acfa81b0bccd4933a648d8f17fcae66957731
henrygosling <henrygarciaospina@hotmail.com>
2022-05-18 12:15:38 -0500

    Práctica componentes Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio-app';
+  public cards:Array<any> = [];
+  ngOnInit(): void {
+     this.cards = [
+       {
+        title:'Vídeo 1',
+        subtitle:'Subtitle Vídeo 1'
+       },
+       {
+        title:'Vídeo 2',
+        subtitle:'Subtitle Vídeo 2'
+       },
+       {
+        title:'Vídeo 3',
+        subtitle:'Subtitle Vídeo 3'
+       }    
+     ]
+  }
 }
